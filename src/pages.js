dacdbe4f2893177520a9b3c4fc3fd4c2c957aacb
Willian Oliveira <wijloc@gmail.com>
2021-03-22 20:38:50 -0300

    fix: remove use of unsupported function replaceAll

--- src/pages.js
+++ src/pages.js
@@ -12,7 +12,7 @@ module.exports = {
           id: polygon.id,
           area: polygon.area,
           name: polygon.name,
-          fileName: polygon.name.replaceAll(" ", "_")
+          fileName: polygon.name.replace(/ /g, '_')
         }
       });
       return res.render("polygons", { polygons });
