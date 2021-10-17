425340e65bfa9505d07082a73998a62ab0557c81
Willian Oliveira <wijloc@gmail.com>
2021-10-17 15:51:55 -0300

    fix: change url of backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL: 'https://polygon-creator-backend.herokuapp.com/',
 });
 
 exports.api = api;
